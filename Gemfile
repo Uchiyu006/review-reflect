1983b5ddb8741787b5f9691519bdb010992daeba
uchi <uchiyu006@gmail.com>
2024-07-28 17:23:20 +0900

    redis, sidekiq設定とbyebug導入

--- Gemfile
+++ Gemfile
@@ -30,7 +30,7 @@ gem "cssbundling-rails"
 gem "jbuilder"
 
 # Use Redis adapter to run Action Cable in production
-# gem "redis", ">= 4.0.1"
+gem "redis"
 
 # Use Kredis to get higher-level data types in Redis [https://github.com/rails/kredis]
 # gem "kredis"
@@ -75,3 +75,4 @@ gem "dockerfile-rails", ">= 1.6", :group => :development
 gem 'dotenv-rails'
 gem 'ruby-openai'
 gem "sidekiq"
+gem 'byebug'
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
+  Sidekiq::Web.use(Rack::Auth::Basic) do |user_id, password|
+    [user_id, password] == [ENV['SIDEKIQ_BASIC_ID'], ENV['SIDEKIQ_BASIC_PASSWORD']]
+  end
+  mount Sidekiq::Web => '/sidekiq'
+  
   root 'staticpages#top'
   resources :users, only: %i[new create]
   resources :reviews
   resources :conversations do
     collection do
-      get 'query'
+      post 'query'
     end
   end
   get 'login', to: 'user_sessions#new'
